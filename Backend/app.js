fc41a531bf1b76ae4a371c1d646d71e94a1033c1
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-04-25 16:24:22 +0530

    created 2 endpoints to get and update available slots

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,37 @@
-const db = require('./config.js');
-const slots =  require('./getTimeSlots.js');
-const slotsArr = slots.slotsArr
-console.log(slotsArr)
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const booking = require("./booking/book-appointment");
+
+const db = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "voice_based_eprescription",
+  },
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    console.log(booking)
+    res.send(JSON.stringify("Hello world"));
+});
+
+app.get("/get-available-slots", (req, res) => {
+  booking.getAvailableSlots(req, res, db);
+});
+
+app.post("/book-slot", (req, res) => {
+  booking.bookSlot(req, res, db);
+});
+
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
