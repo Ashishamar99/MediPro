d3ca3580a6ffa5d115a5fd3dc9832d1b892a7d5a
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-05-24 15:42:50 +0530

    removed transaction

--- src/consultation/book-appointment.js
+++ src/consultation/book-appointment.js
@@ -30,69 +30,64 @@ const bookSlot = (req, res, db) => {
   //get slot details for given slot number
   const { slotNo, pid, did } = req.body;
 
-  db.transaction(function (trx) {
-    db("slots")
-      .where({ slot_no: slotNo, isBooked: 0 })
-      .then((slotArr) => {
-        db("doctor")
-          .where({ isAvailable: 1 })
-          .then((doc) => {
-            if (!slotArr.length || !doc.length) {
-              res.status(400).send("Slot unavailable");
-            } else {
-              let slotDetails = slotArr[0];
-              let startTime = slotDetails.slot_start;
-              let endTime = slotDetails.slot_end;
+  db("slots")
+    .where({ slot_no: slotNo, isBooked: 0 })
+    .then((slotArr) => {
+      db("doctor")
+        .where({ isAvailable: 1 })
+        .then((doc) => {
+          if (!slotArr.length || !doc.length) {
+            res.status(400).send("Slot unavailable");
+          } else {
+            let slotDetails = slotArr[0];
+            let startTime = slotDetails.slot_start;
+            let endTime = slotDetails.slot_end;
 
-              return (
-                trx
-                  .insert({
-                    booking_date: dateTime().format("YYYY-MM-DD"), //assuming booking only for current day
-                    start_time: startTime,
-                    end_time: endTime,
-                    slot_no: slotNo,
-                    pid: pid,
-                    did: did,
-                  })
-                  .into("appointments")
-                  //update available slots and doctor availability
-                  .then(() => {
-                    db("slots")
-                      .where({ slot_no: slotNo })
-                      .update({ isBooked: 1 })
-                      .then(() => {
-                        db("doctor")
-                          .where({ did: did })
-                          .update({ isAvailable: 0 })
-                          .then(() => {
-                            trx.commit;
-                            res.status(200).send("Booking successful");
-                          })
-                          .catch((err) => {
-                            trx.rollback;
-                            res.status(400).send("Couldn't book appointment");
-                            console.error(err);
-                          });
-                      })
-                      .catch((err) => {
-                        trx.rollback;
-                        res.status(400).send("Couldn't book appointment");
-                        console.error(err);
-                      });
-                  })
-                  .catch((err) => {
-                    trx.rollback;
-                    res.status(500).send("Couldn't book appointment");
-                    console.error(err);
-                  })
-              );
-            }
-          });
-      });
-  }).catch(function (err) {
-    res.status(500).send("unable to register");
-    console.error(err);
-  });
+            return (
+              db("appointments")
+                .insert({
+                  booking_date: dateTime().format("YYYY-MM-DD"), //assuming booking only for current day
+                  start_time: startTime,
+                  end_time: endTime,
+                  slot_no: slotNo,
+                  pid: pid,
+                  did: did,
+                })
+
+                //update available slots and doctor availability
+                .then(() => {
+                  db("slots")
+                    .where({ slot_no: slotNo })
+                    .update({ isBooked: 1 })
+                    .then(() => {
+                      db("doctor")
+                        .where({ did: did })
+                        .update({ isAvailable: 0 })
+                        .then(() => {
+                          res.status(200).send("Booking successful");
+                        })
+                        .catch((err) => {
+                          res.status(400).send("Couldn't book appointment");
+                          console.error(err);
+                        });
+                    })
+                    .catch((err) => {
+                      res.status(400).send("Couldn't book appointment");
+                      console.error(err);
+                    });
+                })
+                .catch((err) => {
+                  res.status(500).send("Couldn't book appointment");
+                  console.error(err);
+                })
+            );
+          }
+        });
+    })
+    .catch(function (err) {
+      res.status(500).send("unable to register");
+      console.error(err);
+    });
 };
 
 const unblockAllSlots = (req, res, db) => {
