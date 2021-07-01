5ea24127f0568458fe848060e3c0531eb10fb0aa
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-07-01 16:39:32 +0530

    API to add consultation data

--- src/consultation/consultation.js
+++ src/consultation/consultation.js
@@ -1,6 +1,6 @@
 const getConsultationList = (req, res, db) => {
   db.select("*")
-    .from("consultation")
+    .from("consultations")
     .then((consultations) => {
       res.status(200).send(consultations);
     })
@@ -13,7 +13,7 @@ const getConsultationList = (req, res, db) => {
 const getConsultationWithID = (req, res, db) => {
   const cid = req.params.id;
   db.select("*")
-    .from("consultation")
+    .from("consultations")
     .where("cid", "=", cid)
     .then((consultations) => {
       res.status(200).send(consultations);
@@ -27,7 +27,7 @@ const getConsultationWithID = (req, res, db) => {
 const getPatientConsultation = (req, res, db) => {
   const pid = req.params.id;
   db.select("*")
-    .from("consultation")
+    .from("consultations")
     .where("pid", "=", pid)
     .then((consultations) => {
       res.status(200).send(consultations);
@@ -41,7 +41,7 @@ const getPatientConsultation = (req, res, db) => {
 const getDoctorConsultation = (req, res, db) => {
   const did = req.params.id;
   db.select("*")
-    .from("consultation")
+    .from("consultations")
     .where("did", "=", did)
     .then((consultations) => {
       res.status(200).send(consultations);
@@ -52,9 +52,40 @@ const getDoctorConsultation = (req, res, db) => {
     });
 };
 
+const addConsultationInfo = (req, res, db) => {
+  const speechData = req.body.audio;
+  let formattedSpeechData = "";
+  formattedSpeechData += `Diagnosing for, ${speechData.diagnosis}.`;
+  let medicine = speechData.medicineData.join(".");
+  formattedSpeechData += ` Medicines prescribed, ${medicine}.`;
+  formattedSpeechData += speechData.advice.length
+    ? `Advice, ${speechData.advice}`
+    : "";
+  req.body["audio"] = formattedSpeechData;
+  db.transaction(function (trx) {
+    const consultationInfo = req.body;
+    return trx
+      .insert(consultationInfo)
+      .into("consultations")
+      .then((id) => {
+        trx.commit;
+        res.status(200).send(id);
+      })
+      .catch((err) => {
+        trx.rollback;
+        res.status(500).send("Couldn't complete consultation");
+        console.error(err);
+      });
+  }).catch(function (err) {
+    res.status(400).send("unable to complete consultation");
+    console.error(err);
+  });
+};
+
 module.exports = {
   getConsultationList: getConsultationList,
   getConsultationWithID: getConsultationWithID,
   getPatientConsultation: getPatientConsultation,
   getDoctorConsultation: getDoctorConsultation,
+  addConsultationInfo: addConsultationInfo,
 };
