f577c6fd0ad5252b5ccaa2d068a6bf6518892056
Ashish Amar <ashishamar1999@gmail.com>
2021-07-14 18:06:46 +0530

    DONT MERGE ERROR IN IVR

--- src/ivr.js
+++ src/ivr.js
@@ -1,42 +1,59 @@
 const VoiceResponse = require("twilio").twiml.VoiceResponse;
 const querystring = require("querystring");
 
-//Abhi's Creds.
-// const accountSid = 'ACbe11bf5856751706836158a633466d34';
-// const authToken = 'a0ee05fa5617e158dd302945f36e811c';
+const getNumberCallSendSMS = (message) => {
 
-//Ashish Creds.
-const accountSid = 'AC215b45eb4eda26670a9e4f221311be6e';
-const authToken = '171e378a55f1952515effffd3bd040ee';
-const client = require("twilio")(accountSid, authToken);
+  var number = '';
 
-const sendSMS = (message) => {
+  // const accountSid = 'ACbe11bf5856751706836158a633466d34';
+  // const authToken = 'be9b03f0f05a990d887617b1179de152';
+  const authToken = 'e3b785ff7152aac168b4a15dbd369f02';
+  const accountSid = 'AC215b45eb4eda26670a9e4f221311be6e';
+
+  const client = require("twilio")(accountSid, authToken);
 
   client.calls.list({limit: 1})
-                    .then(calls => calls.forEach(c => {
-                      console.log(`Time = ${c.dateUpdated}, From = ${c.from}`)
+    .then(calls => calls.forEach(c => {
+      console.log(c);
+      // console.log(`Time = ${c.dateUpdated}, From = ${c.from}`);
 
-                      //Send SMS.
-                      var SMSmsg = message;
-                      client.messages
-                            .create({body: SMSmsg, from: c.to, to: c.from})
-                            .then(message => console.log(message.sid));
-                    }));
+      number = c.from.slice(3);
+      //send SMS now
+      sendSMS(message, number);
+      }));
+
+}
+
+const sendSMS = (message, number) => {
+
+  
+  var unirest = require("unirest");
+
+var req = unirest("GET", "https://www.fast2sms.com/dev/bulkV2");
+
+req.query({
+  "authorization": "B9Hy6Fvs3dJUo2cj175PeGOkKEA4TiVNLMWrq0XRhwZl8uCIQYOVePXyYrE6JMbj41iUHvR5owIWsh7x",
+  "sender_id": "TXTIND",
+  "message": message,
+  "route": "v3",
+  "numbers": number
+});
+
+req.headers({
+  "cache-control": "no-cache"
+});
+
+
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
+
+  console.log(res.body);
+});
   
 }
 
-const bookAppointment = (givenPID, res, db) => {
-  console.log(givenPID);
-  sendSMS(`Appointment details for ${givenPID} is : type of PID = ${typeof(givenPID)}`);
-
-  //make query for pid, if pid is there, get available slots for general doctor. get booking confirmation.
-  // voice - available slot is at :start time: to confirm press 1 to cancel press 2
-  // else something.
-}
-
-//pidMenu is like ivrMenu which passes PID input to bookAppointment.
-const pidMenu = (req, res, db) => {
-  var body = "";
+const bookingMenu = (req, res, db) => {
+  let body = "";
   req.on("data", (chunk) => {
     body += chunk;
   });
@@ -48,30 +65,203 @@ const pidMenu = (req, res, db) => {
   res.type("text/xml");
 };
 
-//getPID is like handleIVRRequest. which triggers pidMenu.
-const getPID = (res) => {
-  const phone = new VoiceResponse();
-  const gather = phone.gather({
-    action: "/ivr/pidmenu",
-    // timeout: 5,
+const handleBookingMenu = (digit, pid, res, db) => {
+  if (digit === 1) {
+    //get slots`
+    handleBooking(pid, res, db);
+  } else if (digit === 2) {
+    handleConsultation(pid, res, db);
+    //get recent consultation
+  }
+};
+
+const appointmentMenu = (req, res, db) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", function () {
+    let enteredDigit = parseInt(querystring.parse(body).Digits);
+    let pid = req.params.id;
+    handleAppointment(enteredDigit, pid, res, db);
+  });
+};
+
+const handleAppointment = (digit, pid, res, db) => {
+  const fields = [
+    "General",
+    "Gynecologist",
+    "Neurosurgeon",
+    "Psychiatrist",
+    "Dental",
+  ];
+  let bookingField = fields[digit - 1];
+  console.log("bookingField: ", bookingField);
+  let did = "";
+  db.select("*")
+    .from("doctor")
+    .where({ isAvailable: "1", role: bookingField })
+    .then((doctor) => {
+      did = doctor[0].did;
+      let vr = new VoiceResponse();
+      let result = [];
+      db.select()
+        .from("slots")
+        .then((slotArr) => {
+          slotArr.map((slot) => {
+            if (slot.isBooked == 0) {
+              result.push(slot);
+            }
+          });
+
+          if (result && result.length) {
+            let slot_no = result[0].slot_no;
+
+            db("slots")
+              .where({ slot_no: slot_no, isBooked: 0 })
+              .then((slotArr) => {
+                db("doctor")
+                  .where({ isAvailable: 1 })
+                  .then((doc) => {
+                    if (!slotArr.length || !doc.length) {
+                      vr.say({voice: 'Polly.Aditi'},"Sorry, No slots available at this moment");
+                      res.send(vr.toString());
+                    } else {
+                      let slotDetails = slotArr[0];
+                      let startTime = slotDetails.slot_start;
+                      let endTime = slotDetails.slot_end;
+
+                      return (
+                        db("appointments")
+                          .insert({
+                            booking_date: dateTime().format("YYYY-MM-DD"),
+                            start_time: startTime,
+                            end_time: endTime,
+                            slot_no: slot_no,
+                            pid: pid,
+                            did: did,
+                          })
+
+                          //update available slots and doctor availability
+                          .then(() => {
+                            db("slots")
+                              .where({ slot_no: slot_no })
+                              .update({ isBooked: 1 })
+                              .then(() => {
+                                console.log("Success");
+                                db("doctor")
+                                  .where({ did: did })
+                                  .update({ isAvailable: 0 })
+                                  .then(() => {
+                                    vr.say(
+                                      {voice: 'Polly.Aditi'},
+                                      `Your Booking for ${bookingField} is successful, appointment at ${dateTime(
+                                        startTime,
+                                        "hh:mm a"
+                                      ).format(
+                                        "LT"
+                                      )}, details of booking will be shared to you through SMS.`
+                                    );
+                                    res.send(vr.toString());
+                                  })
+                                  .catch((err) => {
+                                    // vr.say("Couldn't book appointment");
+                                    console.error(err);
+                                  });
+                              })
+                              .catch((err) => {
+                                console.error(err);
+                              });
+                          })
+                          .catch((err) => {
+                            console.error(err);
+                          })
+                      );
+                    }
+                  });
+              })
+              .catch(function (err) {
+                console.error(err);
+              });
+          }
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const handleBooking = (pid, res, db) => {
+  const voiceResponse = new VoiceResponse();
+  const gather = voiceResponse.gather({
+    action: `/ivr/appointment/${pid}`,
+    numDigits: "1",
     method: "POST",
   });
 
-  gather.say(
-    "Enter your P I D",
-    { loop: 2 }
-  );
+  const fields = [
+    "General",
+    "Gynecologist",
+    "Neurosurgeon",
+    "Psychiatrist",
+    "Dental",
+  ];
+  let msg = "";
+  for (let i = 0; i < fields.length; i++) {
+    msg += `To book an appointment for ${fields[i]} press ${i + 1} \n`;
+  }
+  gather.say({voice: 'Polly.Aditi'}, msg, { loop: 2 });
   res.type("text/xml");
   res.send(phone.toString());
 };
 
-const greetUser = (digit, res, db) => {
-  const twiml = new VoiceResponse();
-  var id = "";
-  if (digit === 1) {
-    mymsg = 'You are an existing user';
-    twiml.say(mymsg); //gather PID, if valid PID -> booking, listen pres
-    getPID(res);
+const handleConsultation = (pid, res, db) => {
+  const vr = new VoiceResponse();
+  db("consultations")
+    .where({ pid: pid })
+    .then((data) => {
+      if (data && data.length) {
+        vr.say({voice: 'Polly.Aditi'}, 
+          `Playing your most recent consultation done on ${dateTime(
+            data[0].cdatetime
+          ).format("MMMM Do YYYY")}`
+        );
+        vr.say({voice: 'Polly.Aditi'}, data[0].audio);
+        vr.hangup();
+        res.send(vr.toString());
+      } else {
+        vr.say({voice: 'Polly.Aditi'}, `No consultation data found for P I D ${pid}`);
+        vr.hangup();
+        res.send(vr.toString());
+      }
+    });
+};
+
+const handlePatientRegister = (res, db) => {
+  db.insert({
+    pname: null,
+    ppasswd: null,
+    pemail: null,
+    pphno: null,
+    dob: null,
+    gender: null,
+  })
+    .into("patient")
+    .then((pid) => {
+      let id = String(pid);
+      let msg = `Registration successful, You're P I D is ${id}.`;
+      let twiml = new VoiceResponse();
+      twiml.say({voice: 'Polly.Aditi'}, msg);
+
+      getNumberCallSendSMS(msg);
+
+      const gather = twiml.gather({
+        action: `/ivr/booking-menu/${id}`,
+        numDigits: "1",
+        method: "POST",
+      });
+
+      gather.say({voice: 'Polly.Aditi'}, "To book an appointment press 1", { loop: 2 });
 
   } else if (digit === 2) {
     db.insert({
@@ -89,11 +279,42 @@ const greetUser = (digit, res, db) => {
         twiml.say(msg);
         twiml.hangup();
 
-        smsMessage = `Registration successful, You're P I D is ${id}.`;
-        sendSMS(smsMessage);
+    db.select("pname")
+      .from("patient")
+      .where({ pid: id })
+      .then((data) => {
+        if (data && data.length) {
+          gather.say(
+            {voice: 'Polly.Aditi'},
+            `Welcome ${data[0].pname}, To book an appointment press 1. To hear previous consultation details press 2.`,
+            { loop: 2 }
+          );
 
-        res.send(twiml.toString());
+          res.type("text/xml");
+          res.send(twiml.toString());
+        } else {
+          twiml.say(
+            {voice: 'Polly.Aditi'},
+            `No user found for P I D ${id}. Please check the P I D and try again`
+          );
+          twiml.hangup();
+          res.type("text/xml");
+          res.send(twiml.toString());
+        }
       });
+  });
+};
+
+const handlePatientLogin = (res, db) => {
+  let twiml = new VoiceResponse();
+
+  const gather = twiml.gather({
+    action: "/ivr/login",
+    timeout: 5,
+    method: "POST",
+  });
+
+  gather.say({voice: 'Polly.Aditi'}, "Please enter your P I D.", { loop: 2 });
 
     // console.log(msg);
     // twiml.say("");
@@ -115,7 +336,8 @@ const ivrMenu = (req, res, db) => {
 
 const handleIVRRequest = (req, res, db) => {
   const voiceResponse = new VoiceResponse();
-  voiceResponse.say({ loop: 1 }, "Welcome to voice based e-prescription");
+  voiceResponse.say({ loop: 1,
+    voice: 'Polly.Aditi'}, "Welcome to voice based e-prescription");
   const gather = voiceResponse.gather({
     action: "/ivr/menu",
     numDigits: "1",
@@ -124,6 +346,7 @@ const handleIVRRequest = (req, res, db) => {
   });
 
   gather.say(
+    {voice: 'Polly.Aditi'},
     "Please press 1 if you're an existing user. Press 2 if you're a new user",
     { loop: 2 }
   );
