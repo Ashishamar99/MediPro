fbaed9006aed7e35fe8fe92014fc2068df8b4e60
AbhishekGMV <abhishek.mvg@gmail.com>
2024-03-20 13:11:55 +0530

    remove dist folder

--- dist/src/config/supabase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a, _b;
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-const supabaseUrl = (_a = process.env.SUPABASE_URL) !== null && _a !== void 0 ? _a : "";
-const supabaseKey = (_b = process.env.SUPABASE_KEY) !== null && _b !== void 0 ? _b : "";
-exports.default = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
-//# sourceMappingURL=supabase.js.map
